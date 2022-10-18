0f622017856ae4f62182bebe1bc590c4e266d041
dnjyj1024 <dnjyj1024@naver.com>
2022-10-18 17:11:14 +0900

    06-1

--- main.c
+++ main.c
@@ -3,22 +3,11 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void sub(void);
-
-int main(void) 
+int all_files;
+extern void sub();
+int main(void)
 {
-	int i;
-	for(i=0;i<3;i++)
-	sub();
-	return 0;
-}
-
-void sub(void)
-{
-	int auto_count = 0;
-	static int static_count = 0;
-	auto_count++; // 자동 할당 
-	static_count++; //정적할당  
-	printf("auto_count=%d\n", auto_count);
-	printf("static_count=%d\n", static_count);  
+sub();
+printf("%d\n", all_files);
+return 0;
 }
